111aa049a24d95123607cda4d25b8e61b284a333
HuuBng <holehuubang2005@gmail.com>
2024-08-27 19:41:00 +0700

    Add GameDeck.java

---
+++ LootTheLoop/src/LootTheLoop/GameDeck.java
@@ -0,0 +1,108 @@
+package LootTheLoop;
+
+import java.util.*;
+
+public class GameDeck extends ArrayList<GameCard> {
+
+    ArrayList<GameCard> temple;
+    ArrayList<GameCard> notes;
+    ArrayList<GameCard> score;
+
+    public GameDeck() {
+        temple = new ArrayList<>();
+        notes = new ArrayList<>();
+        score = new ArrayList<>();
+    }
+
+    public void reset() {
+        // Add a joker
+        temple.add(new GameCard(GameCard.Rank.JOKER, GameCard.Suit.JOKER));
+
+        // Add other cards
+        for (int i = 1; i < GameCard.Suit.values().length; i++) {
+            for (int j = 1; j < GameCard.Rank.values().length; j++) {
+                temple.add(new GameCard(GameCard.Rank.values()[j], GameCard.Suit.values()[i]));
+            }
+        }
+    }
+
+    public void shuffle() {
+        Random ran = new Random();
+        int x = ran.nextInt(9) + 2;
+        int i = 0;
+        while (i < x) {
+            Collections.shuffle(temple);
+            i++;
+        }
+
+    }
+
+    public boolean addToNotes(GameCard e) throws IllegalArgumentException {
+        if (e.getFace() == GameCard.Face.DOWN) {
+            System.err.println("Face is DOWN");
+            return false;
+        }
+
+        if (e.getRank() == GameCard.Rank.JOKER) {
+            System.err.println("Can not add JOKER to Notes");
+            return false;
+        }
+
+        if (notes.size() < 3) {
+            notes.add(e);
+            return temple.remove(e);
+        } else {
+            System.err.println("Notes is FULL");
+            return false;
+        }
+    }
+
+    public void addToTemple(GameCard e) {
+        temple.add(e);
+        notes.remove(e);
+    }
+
+    public void addToDiscard(GameCard e) {
+        score.add(e);
+        temple.remove(e);
+    }
+
+    public void list(ArrayList gameList) {
+        gameList.forEach((gc) -> {
+            System.out.println(gc);
+        });
+    }
+
+    public void listAll() {
+        System.out.println("\n--- Temple ---");
+        int ti = 1;
+        for (int i = 0; i < temple.size(); i++) {
+            System.out.println(ti + ": " + temple.get(i));
+            ti++;
+        }
+
+        System.out.println("\n--- Notes ---");
+//        notes.forEach((ngc) -> {
+//            System.out.println(ngc);
+//        });
+
+        int ni = 1;
+        for (int i = 0; i < notes.size(); i++) {
+            System.out.println(ni + ": " + notes.get(i));
+            ni++;
+        }
+    }
+
+    public void actLookAround() {
+        if (temple.get(temple.size() - 1).getFace() == GameCard.Face.DOWN) {
+            temple.get(temple.size() - 1).faceUp();
+            temple.get(temple.size() - 2).faceUp();
+        } else {
+            System.err.println("Top card is face UP");
+        }
+    }
+
+    public void actExplore(GameCard e) {
+
+    }
+}
