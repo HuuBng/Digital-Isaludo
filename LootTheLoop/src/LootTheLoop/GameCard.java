c8dab6a21483b74f42a7ef889a6dddfbbc567256
HuuBng <holehuubang2005@gmail.com>
2024-08-27 19:39:23 +0700

    Add GameCard.java

---
+++ LootTheLoop/src/LootTheLoop/GameCard.java
@@ -0,0 +1,55 @@
+package LootTheLoop;
+
+public class GameCard {
+
+    enum Suit {
+        JOKER, HEART, DIAMOND, CLUB, SPADE;
+    }
+
+    enum Rank {
+        JOKER, ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING;
+    }
+
+    enum Face {
+        UP, DOWN;
+    }
+
+    private Rank rank;
+    private Suit suit;
+    private Face face;
+
+    public GameCard() {
+    }
+
+    public GameCard(Rank rank, Suit suit) {
+        this.rank = rank;
+        this.suit = suit;
+        this.face = Face.DOWN;
+    }
+
+    public void faceUp() {
+        this.face = Face.UP;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public Face getFace() {
+        return face;
+    }
+
+    @Override
+    public String toString() {
+        if (face == Face.DOWN) {
+            return "--- of --- face " + face;
+        } else {
+            return rank + " of " + suit + " face " + face;
+        }
+    }
+
+}
